9ad567ee4f19baad2d1a228828e8403cd3af69f5
NurdauletAlibay <alibay.n01@mail.ru>
2022-10-19 15:59:08 +0600

    length

---
+++ length.py
@@ -0,0 +1,10 @@
+''' length методы
+list = [1, 2, 3, 4, 5]
+print(len(list))
+
+dict = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5}
+print(len(dict))
+
+tup = (1, 2, 3, 4, 5)
+print(len(tup))
+'''
