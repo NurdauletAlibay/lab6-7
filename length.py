0fc7e829a6795ae38bb7c208f80ebc778aea6d48
NurdauletAlibay <alibay.n01@mail.ru>
2022-10-19 16:05:03 +0600

    length

--- length.py
+++ length.py
@@ -11,4 +11,30 @@ print(len(tup))
 set = {1, 2, 3, 4, 5}
 length = len(set)
 print(length)
-'''
+
+#clear методы
+list = [1,2,3,4,5]
+element = list.clear()
+print(element)
+
+dict = {"one":1,"two":2,"three":3,"four":4,"five":5}
+element = dict.clear()
+print(element)
+
+set = {1,2,3,4,5}
+set.clear()
+print(set)
+
+tup = (1,2,3,4,5)
+del tup
+
+#элементті өшіру
+list = [1,2,3,4,5]
+print(list.remove(3))
+
+set = {1, 2, 3, 4, 5, 6}
+print(set.remove(3))
+
+dict = {"one":1,"two":2,"three":3,"four":4,"five":5}
+dict.pop("two")
+...
